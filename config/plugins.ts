b00e0a3cb3c6efe9b08917a3f383c850bbb205eb
InnerDesires <vlad.tertyshny@gmail.com>
2025-01-10 11:04:39 +0200

    feat: initial, added translations

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,14 @@
-export default () => ({});
+export default () => ({
+    email: {
+        config: {
+          provider: 'strapi-provider-email-resend',
+          providerOptions: {
+            apiKey: process.env.RESEND_API_KEY, // Required
+          },
+          settings: {
+            defaultFrom: 'me@example.com',
+            defaultReplyTo: 'me@example.com',
+          },
+        }
+      },
+});
